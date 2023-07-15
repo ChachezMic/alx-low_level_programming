f4448fe1dbda55212090d699f0a519c852ba0305
ChachezMic <chachezmi@gmail.com>
2023-07-14 19:28:22 -0700

    lu it is

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long int: %zu bytes\n", sizeof(long int));
-	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of int: %lu bytes\n", sizeof(int));
+	printf("Size of long int: %lu bytes\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of float: %lu bytes\n", sizeof(float));
 
 	return (0);

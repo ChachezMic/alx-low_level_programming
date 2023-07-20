1ffd97c1b2f5d6556a7a5d84804d66202fb96375
ChachezMic <chachezmi@gmail.com>
2023-07-20 16:42:43 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Entry point
- * #c - character to be checked
+ * @c:character to be checked
  * description:checks for lowercase characters
  * Return: 1 for lowercase character or 0 for anything else
  */

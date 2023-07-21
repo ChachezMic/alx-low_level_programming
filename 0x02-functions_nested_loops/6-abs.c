d3f56dd12d1e9d9df6979eb6e00685a4c478db1d
ChachezMic <chachezmi@gmail.com>
2023-07-20 18:34:49 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * abs - Entry point
+ * description:computes absolute value of an integer
+ * @c:character to be computed
+ * Return:Absolute value of a number or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}

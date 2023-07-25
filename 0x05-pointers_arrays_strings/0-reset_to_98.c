eafe24c4022d5fb499241c6cf6696ad50c48749c
ChachezMic <chachezmi@gmail.com>
2023-07-24 18:58:18 -0700

    arrays start

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets value to 98
+ * @*n:points to the vaue of the address updated
+ * Return:void
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+	
+	*p = 98;
+}

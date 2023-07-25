5ba41f035857826d0855390d2aefd41fce9c5560
ChachezMic <chachezmi@gmail.com>
2023-07-24 23:51:06 -0700

    computers reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
 		_putchar(*s++);
 	{
-	for (i = -1; s[i] != 0; i--)
+	for (i = -1; s[i] >= 0; i--)
 		_putchar(s[i]);
 	}
 	_putchar('\n');

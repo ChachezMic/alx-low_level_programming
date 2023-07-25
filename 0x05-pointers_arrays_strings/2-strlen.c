89ead36adecc75d267e6f5edde1b9084aa44f7e2
ChachezMic <chachezmi@gmail.com>
2023-07-24 21:27:55 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - calculates the length of the string
+ * @s:points to the string
+ * Return:size of string
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	;
+}
+return (i);
+}

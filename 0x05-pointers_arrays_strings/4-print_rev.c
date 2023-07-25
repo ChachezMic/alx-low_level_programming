e07d00072409d08e8568bb040f2959220b926cd5
ChachezMic <chachezmi@gmail.com>
2023-07-24 23:34:11 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s:to be reversed
+ * return:void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+		_putchar(*s++);
+	{
+	for (i = -1; s[i] != 0; i--)
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

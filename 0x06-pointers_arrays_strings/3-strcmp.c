b05ca6d1310c1ef4e0f00b3f2f228cc89a5c67b8
ChachezMic <chachezmi@gmail.com>
2023-07-26 10:28:02 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,8 @@
 
 int _strcmp(char *s1, char  *s2)
 {
-	int i, output;
+	int result;
 
-	i = 0;
-
-	while (*(s1 + i) == *(s2 + i) && *(s2 + i) && *(s1 + i) != '\0')
-		i++;
-	output = (*(s1 + i) - *(s2 + i));
-	return (output);
-} 
+result = strcmp(s1, s2);
+return (result);
+}

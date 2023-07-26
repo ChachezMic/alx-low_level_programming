9f7b9c3bdf2c17f95d472625d68c290d404e11a4
ChachezMic <chachezmi@gmail.com>
2023-07-25 17:14:22 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- *puts2- print skip one number
- *@str:checked
- *Return:nothing
+ * puts2 - prints every other character of the string
+ * @str:string to be checked
+ * Return:void
  */
 void puts2(char *str)
 {
+	int i;
 
-	int i = 0;
-
-	while (*(str + i) != '\0')
+	for (i = 0; *(str + i) != '\0'; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }

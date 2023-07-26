d1aefd38cce05cee588eae32ddea5f8a56c04017
ChachezMic <chachezmi@gmail.com>
2023-07-25 18:45:11 -0700

    6 puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+
 /**
  *puts2- print skip one number
  *@str:checked
@@ -6,28 +8,11 @@
  */
 void puts2(char *str)
 {
+	int len, i;
 
-	int longi = 0;
-
-	int t = 0;
-
-	char *y = str;
-
-	int 0;
-
-	while(*y != '\0')
-	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for(o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-		{
-			_putchar(str[o]);
-		}
-	}
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
 

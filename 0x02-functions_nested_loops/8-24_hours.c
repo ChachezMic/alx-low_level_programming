2379ac0b3967a5b28ab31d0e5582cc3754f7886e
ChachezMic <chachezmi@gmail.com>
2023-08-07 06:41:00 -0700

    8-24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 
-	/**
-	 * jack_bauer - prints every minute of the day of Jack Bauer
-	 * starting from 00:00 to 23:59
-	 */
-	void jack_bauer(void)
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * description:starting from 00:00 to 23:59
+ * return: void
+ */
+void jack_bauer(void)
 	{
 		int i, j;
 

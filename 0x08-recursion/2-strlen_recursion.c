57a2e9677e48f527bcc437779796f96717a1ef6a
ChachezMic <chachezmic@gmail.com>
2023-08-30 18:40:24 -0700

    length measured recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strlen_recursion - measures the length of a string
+ * @s:string to be measured
+ * return:length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int size;
+
+	size = 0;
+
+	if (*s)
+	{
+		size++;
+
+		size += _strlen_recursion(s + 1);
+	}
+
+		return (size);
+}
+
+

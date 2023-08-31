fcc618d990dcb7eb434b10e3eaf95253072ef9e2
ChachezMic <chachezmic@gmail.com>
2023-08-31 00:06:34 -0700

    revisit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,17 +7,12 @@
 int factorial(int n)
 {
 	if (n < 1)
-	{
 		return (-1);
-	}
-	else if
-		(n == 0)
-		{
-			return (1);
-		}
-	else
-	{
-		return (n * factorial(n - 1));
-				}
-				}
+	else if (n == 0)
+		return (1);
 
+	else if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}

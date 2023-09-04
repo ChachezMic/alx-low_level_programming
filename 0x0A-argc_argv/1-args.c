e7960a452c82fd3649f221b8b1638d629c7dc771
ChachezMic <chachezmic@gmail.com>
2023-09-04 06:55:44 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,9 @@
  * @argv:array of arguments
  * Return:Always (0) success
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[i])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }

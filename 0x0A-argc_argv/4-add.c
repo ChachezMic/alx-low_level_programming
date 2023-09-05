48cb66b191483384548ea540ad1c1cee543a7067
ChachezMic <chachezmic@gmail.com>
2023-09-04 21:30:17 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc:counts number of arguments
+ * @argv:array of integers
+ * Return:0 for sum of +ve integers,1 if args are digits
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int a;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			int num = atoi(argv[a]);
+
+			if (num == 0 && argv[a][0] != '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += num;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}

e5a1020329fdea36a7123dbe9525e7802f5f04ce
ChachezMic <chachezmic@gmail.com>
2023-09-06 19:41:19 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,28 +9,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int a;
+	int sum, i, a, num;
+	char *tmp;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
+	sum = 0;
+	if (argc > 1)
 	{
 		for (a = 1; a < argc; a++)
 		{
-			int num = atoi(argv[a]);
-
-			if (num == 0 && argv[a][0] != '0')
+			tmp = argv[a];
+			for (i = 0; tmp[i] != '\0'; i++)
 			{
-				printf("Error\n");
-				return (1);
+				if (tmp[i] < 48 || tmp[i] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			num = atoi(tmp);
 			sum += num;
 		}
-		printf("%d\n", sum);
-		return (0);
 	}
+	printf("%d\n", sum);
 	return (0);
 }

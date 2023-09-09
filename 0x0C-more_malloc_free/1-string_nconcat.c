843b197b105e454abf9889105122c4f268629de7
ChachezMic <chachezmic@gmail.com>
2023-09-08 17:05:24 -0700

    drama

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptS1;
-	int length1, length2, totalLength, i, j;
+	unsigned int length1, length2, totalLength, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -30,7 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		ptS1 = malloc(length1 + n);
+		totalLength = (length1 + n);
+		ptS1 = malloc((totalLength + 1) * sizeof(char));
 		if (ptS1 == NULL)
 			return (NULL);
 	}
@@ -38,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptS1[i] = s1[i];
 	}
-	for (j = 0; j < length2; j++)
+	for (j = 0; j < n; j++)
 	{
 		ptS1[i + j] = s2[j];
 	}

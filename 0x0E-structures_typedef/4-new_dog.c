5cab699e0148536d2924250e0778e52c2d5930bc
ChachezMic <chachezmic@gmail.com>
2023-09-12 19:49:48 -0700

    4-new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,12 +20,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog->name = strdup(name);
 	if (newDog->name == NULL)
 	{
+		free(newDog->name);
 		free(newDog);
 		return (NULL);
 	}
 	newDog->owner = strdup(owner);
 	if (newDog->owner == NULL)
 	{
+		free(newDog->owner);
 		free(newDog);
 		return (NULL);
 	}

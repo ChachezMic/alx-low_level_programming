c534bda188d7ed01b3318dea053fa00f87ca9ff0
ChachezMic <chachemic@gmail.com>
2023-09-21 19:26:51 -0700

    trail singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 
 /**
  * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t 
+ * @h: pointer to the list_t
  * Return:no of nodes printed
  */
 size_t print_list(const list_t *h)

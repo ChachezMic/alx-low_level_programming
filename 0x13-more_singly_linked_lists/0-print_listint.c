87e04d78357810a933fc40773026093e5e6ba1aa
ChachezMic <chachemic@gmail.com>
2023-09-26 17:34:48 -0700

    elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of listint_t 
+ * @h: linked list of listint_t
  * Return: no of nodes
  */
 size_t print_listint(const listint_t *h)

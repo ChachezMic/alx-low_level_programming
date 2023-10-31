c80c4aee03855a18bb51ac9687a3b63e65d4f00a
ChachezMic <chachezmic@gmail.com>
2023-10-31 07:47:24 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		text_content = " ";
+		text_content = "";
 	for (ltrsn = 0; text_content[ltrsn]; ltrsn++)
 	;
 	wrt = write(fd, text_content, ltrsn);

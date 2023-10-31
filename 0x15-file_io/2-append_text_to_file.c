83c679479bd5e35dd99d562bec2bccb657c60185
ChachezMic <chachezmic@gmail.com>
2023-10-31 09:42:02 -0700

    hello wrld

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,8 +2,8 @@
 /**
  * append_text_to_file - appends text to a file
  *
- * filename: name of file to be appended
- * text_content:content to add to the file
+ * @filename: name of file to be appended
+ * @text_content:content to add to the file
  * Return:1 on succes and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
